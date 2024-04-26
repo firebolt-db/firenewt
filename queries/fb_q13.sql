bea3fccf8232dbfdace7c8e7ecd0996c6af703cc
Hiren Patel <144179173+h27patel@users.noreply.github.com>
2024-04-26 15:45:52 -0700

    Update fb_q13.sql
    fix formatting

--- queries/fb_q13.sql
+++ queries/fb_q13.sql
@@ -1,1 +1,10 @@
-with busiest_days as ( select visitdate, count(*) from uservisits group by 1 order by 2 desc limit 10 ) select countrycode, avg(length(searchword)) from uservisits where visitdate in (select visitdate from busiest_days) group by countrycode;
+with busiest_days as ( 
+  select visitdate, count(*) 
+  from uservisits 
+  group by 1 
+  order by 2 desc limit 10 
+  ) 
+  select countrycode, avg(length(searchword)) 
+  from uservisits 
+  where visitdate in (select visitdate from busiest_days) 
+  group by countrycode;
