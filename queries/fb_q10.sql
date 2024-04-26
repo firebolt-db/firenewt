f85c7754b020c8b79de185840aa31cbc2fdba380
Hiren Patel <144179173+h27patel@users.noreply.github.com>
2024-04-26 15:42:12 -0700

    Update fb_q10.sql
    fix formatting

--- queries/fb_q10.sql
+++ queries/fb_q10.sql
@@ -1,1 +1,3 @@
-SELECT max(visitdate) as "latest_visit" FROM uservisits WHERE (visitdate >= '2036-02-28' AND visitdate <= DATE_ADD('DAY', 1, '2036-02-28'));
+SELECT max(visitdate) as "latest_visit" 
+  FROM uservisits 
+  WHERE (visitdate >= '2036-02-28' AND visitdate <= DATE_ADD('DAY', 1, '2036-02-28'));
