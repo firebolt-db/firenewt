3ecc337f9e4d44148bfca44ac2eec22190021d19
Judson Wilson <judson@firebolt.io>
2024-09-15 03:21:44 -0700

    Add option for SDK and raw HTTP query requests to powerrun runner. (#7)
    Before this change, run_firenewt_powerrun.py ran all of the benchmark
    queries using raw HTTP requests.
    
    This HTTP client isn't working correctly for long ingest queries
    - for some reason it does not return with the response when the
    query is done. However, the SDK works correctly.
    
    However, this utility stores JSON in the query_label, and the SDK
    does not work correctly in that case.
    
    This change uses the SDK by default. To work around the issues with JSON
    in the query label, it base64 encodes the query lables, with a prefix
    "b64-" in front. Then the SQL queries to read the results from
    INFORMATION_SCHEMA.engine_query_history were updated to decode the
    base64.
    
    To use raw HTTP requests, the CLI option "--use-http-queries" must be
    passed. This might provide certain performance improvements, for
    instance.

--- tools/run_firenewt_powerrun.py
+++ tools/run_firenewt_powerrun.py
@@ -1,4 +1,5 @@
 import argparse
+import base64
 import pandas as pd
 import json
 import firebolt.db
@@ -10,7 +11,7 @@ import time
 
 import httpx
 
-def run_powerrun(query_history, client_id, client_secret, account_name, engine_name, database, api_endpoint):
+def run_powerrun(query_history, client_id, client_secret, account_name, engine_name, database, api_endpoint, raw_http_query=False):
     df = pd.read_csv(query_history)
     df.sort_values("query_start_ts", inplace=True)
     connection = firebolt.db.connect(
@@ -31,39 +32,81 @@ def run_powerrun(query_history, client_id, client_secret, account_name, engine_n
     start_time = None
     characters = string.digits
     random_number = "".join(random.choice(characters) for _ in range(6))
-    run_id = f"powerrun_{random_number}_{time.strftime("date_%Y_%m_%d_time_%H_%M_%S")}"  
+    run_id = f'powerrun_{random_number}_{time.strftime("date_%Y_%m_%d_time_%H_%M_%S")}'
     print(f" Run id is {run_id}")
-    with httpx.Client(timeout=httpx.Timeout(7200.0)) as client:  
+
+    with httpx.Client(timeout=httpx.Timeout(7200.0)) as client:
         for index, row in df.iterrows():                
-            query_label = json.dumps({"rid": run_id, "sql_id": row["query_id"]})            
-            engine_url = connection.engine_url + f"?advanced_mode=1&result_cache_max_bytes_item=0&database={database}&engine={engine_name}&query_label={query_label}"
-            response = client.post(engine_url, data=row["query_text"], headers=headers)
+            query_label_decoded = json.dumps({"rid": run_id, "sql_id": row["query_id"]})
+            query_label = f'b64-{base64.b64encode(query_label_decoded.encode()).decode()}'
+
+            if raw_http_query:
+                engine_url = connection.engine_url + f"?advanced_mode=1&result_cache_max_bytes_item=0&database={database}&engine={engine_name}&query_label={query_label}"
+                t1 = time.time()
+                response = client.post(engine_url, data=row["query_text"], headers=headers)
+            else:
+                cursor.execute(f'set query_label={query_label}')
+                t1 = time.time()
+                # skip_parsing significantly speeds up the client side part of this query. This can greatly affect
+                # certain cases, such as INSERT queries with 1000 literal row VALUES.
+                cursor.execute(row["query_text"], skip_parsing=True)
+
             if start_time is None and "system" not in row["query_id"]:
-                start_time = time.time()
+                start_time = t1
+        cursor.execute("set query_label=''")
     wall_clock_time = time.time() - start_time
 
-    cursor.execute(f"""select 
-	                        trim('"' from JSON_EXTRACT(query_label, '/sql_id', 'JSONPointer')) as sql_id,
-                            round(duration_us/1000000.0,3) as "duration, s"
-                        from information_schema.engine_query_history 
-                        where 
-                            JSON_EXTRACT(query_label, '/rid', 'JSONPointer') = '"{run_id}"'
-  	                        and JSON_EXTRACT(query_label, '/sql_id', 'JSONPointer') not like '"system%' 
-                            and status = 'ENDED_SUCCESSFULLY'                        
-                        order by start_time""") 
+    # Give engine_query_history time to populate
+    time.sleep(10)
+
+    sql = f"""\
+        WITH augmented_history as (
+            select
+            CASE WHEN query_label like 'b64-%' THEN
+                CONVERT_FROM(DECODE(regexp_extract(query_label, 'b64-(.*)', '', 1), 'BASE64'), 'utf8')
+            ELSE
+                ''
+            END as query_label_decoded,
+            *
+            from information_schema.engine_query_history
+        )
+        select
+            trim('"' from JSON_EXTRACT(query_label_decoded, '/sql_id', 'JSONPointer')) as sql_id,
+            round(duration_us/1000000.0,3) as "duration, s"
+        from augmented_history
+        where
+            JSON_EXTRACT(query_label_decoded, '/rid', 'JSONPointer') = '"{run_id}"'
+            and JSON_EXTRACT(query_label_decoded, '/sql_id', 'JSONPointer') not like '"system%'
+            and status = 'ENDED_SUCCESSFULLY'
+            and query_text not ilike 'select 1'
+        order by start_time"""
+    cursor.execute(sql)
     results = cursor.fetchall()
     df_query_history = pd.DataFrame(results, columns=["sql_id", "duration"])
     print(df_query_history.to_markdown(index=False))
     print(f"Wall clock test duration: {wall_clock_time:.2f} seconds")
-    cursor.execute(f"""select 
-                        round(SUM(duration_us/1000000.0),3) as total_duration,
-  					    round(POW(2.718281828459045,(SUM(LN(duration_us/1000000.0)) / COUNT(*))),3) AS geometric_mean
-                from information_schema.engine_query_history 
-                where 
-  					JSON_EXTRACT(query_label, '/rid', 'JSONPointer') = '"{run_id}"'
-  	                and JSON_EXTRACT(query_label, '/sql_id', 'JSONPointer') not like '"system%' 
-                    and status = 'ENDED_SUCCESSFULLY'  
-                group by all""")
+    sql = f"""
+        WITH augmented_history as (
+            select
+            CASE WHEN query_label like 'b64-%' THEN
+                CONVERT_FROM(DECODE(regexp_extract(query_label, 'b64-(.*)', '', 1), 'BASE64'), 'utf8')
+            ELSE
+                ''
+            END as query_label_decoded,
+            *
+            from information_schema.engine_query_history
+        )
+        select
+            round(SUM(duration_us/1000000.0),3) as total_duration,
+            round(POW(2.718281828459045,(SUM(LN(duration_us/1000000.0)) / COUNT(*))),3) AS geometric_mean
+        from augmented_history
+        where
+            JSON_EXTRACT(query_label_decoded, '/rid', 'JSONPointer') = '"{run_id}"'
+            and JSON_EXTRACT(query_label_decoded, '/sql_id', 'JSONPointer') not like '"system%'
+            and status = 'ENDED_SUCCESSFULLY'
+            and query_text not ilike 'select 1'
+        group by all"""
+    cursor.execute(sql)
     results = cursor.fetchall()
     print(f"Total duration for all queries: {results[0][0]:.2f} seconds")
     print(f"Geometric mean of query durations: {results[0][1]:.2f} seconds")
@@ -87,6 +130,11 @@ if __name__ == "__main__":
         help="Optional path to query history file",
         default="../SQL/queries/firenewt_1tb_powerrun.csv",
     )
+    parser.add_argument(
+        "--raw-http-queries",
+        action=argparse.BooleanOptionalAction,
+        help="If set, uses a lightweight raw HTTP query instead of the python SDK."
+    )
     args = parser.parse_args()
     env_variables_set = True
     env_variables_set = env_variables_set and check_env_variable("FB_CLIENT_ID")
@@ -103,5 +151,6 @@ if __name__ == "__main__":
         database=os.environ["FB_DATABASE"]
         api_endpoint=os.environ.get("FB_API")
         if api_endpoint is None:
-            api_endpoint = "api.app.firebolt.io"      
-        run_powerrun(args.query_history, client_id, client_secret, account_name, engine_name, database, api_endpoint)
+            api_endpoint = "api.app.firebolt.io"
+        run_powerrun(args.query_history, client_id, client_secret, account_name, engine_name, database, api_endpoint,
+                     raw_http_query=args.raw_http_queries)
